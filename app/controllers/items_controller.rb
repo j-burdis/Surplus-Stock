07eee3434ed04cfb9fedd459f7f9b05ae9775329
Jonathan Burdis <jonnyburdis@hotmail.com>
2025-01-07 15:06:28 +0000

    create basic show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,16 @@
 class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:show]
   def index
     @items = Item.all
   end
 
   def show
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
