b9f73a938a4c8214f87a074a4f3c67577e45d547
Jonathan Burdis <jonnyburdis@hotmail.com>
2025-01-11 10:37:52 +0000

    add navbar dropdown and icons

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  const menuButton = document.getElementById("menu-button");
+  const menuDropdown = document.getElementById("menu-dropdown");
+
+  menuButton.addEventListener("click", () => {
+    menuDropdown.classList.toggle("hidden");
+  });
+
+  // Close dropdown when clicking outside
+  document.addEventListener("click", (event) => {
+    if (!menuButton.contains(event.target) && !menuDropdown.contains(event.target)) {
+      menuDropdown.classList.add("hidden");
+    }
+  });
+});
