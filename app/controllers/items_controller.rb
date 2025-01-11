b0e2fbde9f7dd799aebc6c252876d19eaffc45c8
Jonathan Burdis <jonnyburdis@hotmail.com>
2025-01-11 17:44:42 +0000

    add price and name sorting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,30 @@ class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   before_action :set_item, only: [:show]
   def index
-    if params[:query].present?
-      @items = Item.search_by_name_and_description(params[:query])
-    else
-      @items = Item.all
+    # if params[:query].present?
+    #   @items = Item.search_by_name_and_description(params[:query])
+    # else
+    #   @items = Item.all
+    # end
+    @items = Item.all
+
+    # Search functionality
+    @items = @items.where("name ILIKE ?", "%#{params[:query]}%") if params[:query].present?
+
+    # Sorting functionality
+    case params[:sort_by]
+    when 'price_asc'
+      @items = @items.order(price: :asc)
+    when 'price_desc'
+      @items = @items.order(price: :desc)
+    when 'name_asc'
+      @items = @items.order(name: :asc)
+    when 'name_desc'
+      @items = @items.order(name: :desc)
     end
+
+    # If no results, show a message
+    flash.now[:notice] = "No results found" if @items.empty?
   end
 
   def show
