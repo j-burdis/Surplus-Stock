d096831c1b3b73dc0678f87c407e80facd7fdacc
Jonathan Burdis <jonnyburdis@hotmail.com>
2025-01-17 11:52:45 +0000

    format wishlist, item show page and navbar

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,13 +1,6 @@
 class WishlistsController < ApplicationController
-  before_action :set_wishlist
-
   def show
+    @wishlist = current_user.wishlist
     @wishlist_items = @wishlist.wishlist_items.includes(:item)
-  end
-
-  private
-
-  def set_wishlist
-    @wishlist = current_user.wishlist || current_user.create_wishlist
   end
 end
