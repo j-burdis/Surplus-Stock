8ab06195a8b3717b25d787ac60ddc36e7dd0c65a
Jonathan Burdis <jonnyburdis@hotmail.com>
2025-02-03 14:12:42 +0000

    add tests for basket items

--- test/models/basket_item_test.rb
+++ test/models/basket_item_test.rb
@@ -4,4 +4,34 @@ class BasketItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @user = users(:alice)
+    @item = items(:table_lamp)
+    @basket = @user.basket
+    @basket_item = BasketItem.create(
+      basket: @basket,
+      item: @item,
+      quantity: 2
+    )
+  end
+
+  test "basket item is valid" do
+    assert @basket_item.valid?
+  end
+
+  test "quantity must be positive" do
+    @basket_item.quantity = 0
+    assert_not @basket_item.valid?
+  end
+
+  test "expired? method works" do
+    old_basket_item = BasketItem.create(
+      basket: @basket,
+      item: @item,
+      quantity: 1,
+      created_at: 1.hour.ago
+    )
+    assert old_basket_item.expired?
+  end
 end
